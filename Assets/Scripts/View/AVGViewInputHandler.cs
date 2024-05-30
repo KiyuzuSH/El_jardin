a57a92f6f597e4742eded7333a9ac71a5b2daea8
Nianyi Wang <wangnianyi2001@outlook.com>
2024-05-31 02:45:56 +0800

    Implement ending dialogue line logic

---
+++ Assets/Scripts/View/AVGViewInputHandler.cs
@@ -0,0 +1,37 @@
+using UnityEngine;
+
+namespace KiyuzuDev.ITGWDO.View {
+	/// <summary>
+	/// A delegate layer for handling player inputs in the AVG view.
+	/// </summary>
+	/// <remarks>
+	/// Author: Nianyi Wang
+	/// Creation time: 05/31/2024 02:34
+	/// </remarks>
+	[RequireComponent(typeof(AVGView))]
+	public class AVGViewInputHandler : MonoBehaviour {
+		#region Message handlers
+		protected void OnEndLine() {
+			EndLine();
+		}
+		#endregion
+
+		#region Interfaces
+		/// <summary>
+		/// End the current line and trigger the rest logic.
+		/// </summary>
+		/// <remarks>
+		/// If the current line is rolling, immediately show all texts.</remarks>
+		public void EndLine() {
+			var dialogue = Core.DialogueManager.Instance;
+			if(dialogue == null) {
+				Debug.LogWarning("Warning: Cannot end line because no dialogue manager is present.");
+				return;
+			}
+
+			dialogue.MoveNextLine();
+			dialogue.ProcessLine();
+		}
+		#endregion
+	}
+}
